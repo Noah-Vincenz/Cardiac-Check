f1a30cbb92291d8fe98b175750f7edb3084a8f23
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2018-01-23 13:03:12 +0000

    Managed to update the table with the initial patient selected. Also cleaned up the code, the website layout and moved all script code inside index.js.

--- public/index.js
+++ public/index.js
@@ -8,9 +8,51 @@ var patientsSelection = document.getElementById("patients_selection");
 const db = firebase.database();
 const patientsRef = db.ref("patients");
 
+window.onload = function () {
+    var limit = 30;    //increase number of dataPoints by increasing the limit
+    var y = 0;
+    var data = [];
+    var dataSeries = { type: "line", color: "red" };
+    var dataPoints = [];
+    for (var i = 0; i < limit; i += 0.1) {
+        y = Math.round(Math.random() * 5);
+        dataPoints.push({
+            x: i,
+            y: y
+        });
+    }
+    dataSeries.dataPoints = dataPoints;
+    data.push(dataSeries);
+
+    //Better to construct options first and then pass it as a parameter
+    var options = {
+        zoomEnabled: true,
+        animationEnabled: true,
+        title: {
+            text: "ECG"
+        },
+        axisX: {
+            labelAngle: 30,
+            title: "Time (seconds)"
+        },
+        axisY: {
+            includeZero: false,
+            title: "Voltage (microVolts)"
+        },
+        data: data  // random data
+    };
+
+    $("#chartContainer").CanvasJSChart(options);
+    changeDataShown("Henry");
+
+}
+
+// this is called when select item changes; the table is updated with the selected patient's data
 function changeDataShown(strUser) {
-    alert("changing data shown");
-    //var tableRef = document.getElementById('my_table');//.getElementsByTagName('table_body');
+    //we only want one row
+    if ($('#my_table tr').length == 2) {
+        document.getElementById("my_table").deleteRow(1);
+    }
     patientsRef.orderByChild("name").equalTo(strUser).on("value", function(snapshot) { // patients
 
         snapshot.forEach(function(data) { // = corresponding patient ie. patient2
@@ -37,13 +79,14 @@ function changeDataShown(strUser) {
 
             });
             var str = "<tr><td>"+id+"</td><td>"+name+"</td><td>"+dob+"</td><td>"+weight+"</td></tr>";
+            $("#table_body").append(str);
 
-            $("#my_table").append(str);
 
         });
 
 
     });
+
 }
 
 
@@ -58,7 +101,12 @@ patientsRef.on("child_added", function(snapshot) {
     patientsSelection.appendChild(opt);
 });
 
-
+// gets called when Select item changes and when send button is pressed
+function getSelectedUser() {
+    var e = document.getElementById("patients_selection");
+    var strUser = e.options[e.selectedIndex].text;
+    return strUser;
+}
 
 function submitText(recipient) {
 
