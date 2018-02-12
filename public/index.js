58111c1485f48dbe3eb3dcf4f684d330a9fe11d3
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2018-02-12 13:16:06 +0000

    integrated firebase storage in my project. Now I am pulling the ECG data that is stored as a .txt file on firebase storage and I am showing this data in the graph. I had difficulties integrating firebase storage, as I had to set the security rules to public and also had to add a JSON file for its CORS, in order to read files on storage and get a code of 200 and not 0 (interrupted). Next I will integrate python to analyse and process the ecg signal.

--- public/index.js
+++ public/index.js
@@ -1,20 +1,21 @@
-
+//import firebase from 'firebase'
+//import 'firebase/storage'
+//import "firebase/storage";
 
 var textArea = document.getElementById("textArea");
 var sendButton = document.getElementById("sendButton");
 var tableBody = document.getElementById("table_body");
 var patientsSelection = document.getElementById("patients_selection");
+var xAxisStripLinesArray = [];
+var yAxisStripLinesArray = [];
 
 const db = firebase.database();
 const patientsRef = db.ref("patients");
 const recordsRef = db.ref("datarecords");
+const storage = firebase.storage();
+
 
 window.onload = function () {
-    var limit = 1000;    //increase number of dataPoints by increasing the limit
-    var y = 0;
-    var data = [];
-    var dataSeries = { type: "line", color: "red" };
-    var myDataPoints = [];
     changeDataShown("Henry");
 }
 
@@ -25,7 +26,6 @@ function changeDataShown(strUser) {
         document.getElementById("my_table").deleteRow(1);
     }
     patientsRef.orderByChild("name").equalTo(strUser).on("value", function(snapshot) { // patients
-
         snapshot.forEach(function(data) { // = corresponding patient ie. patient2
             updateGraph(data.key);
             var id;
@@ -61,63 +61,147 @@ function changeDataShown(strUser) {
 }
 
 function updateGraph(patientKey) {
-    recordsRef.once("value")
-    .then(function(snapshot) {
-        var key = snapshot.key; // "ada"
-        snapshot.forEach(function(data) { // = corresponding patient ie. patient2
-            if (data.key == patientKey) {
-                sampleECG = data.val();
-                var limit = 1000;    //increase number of dataPoints by increasing the limit
-                var y = 0;
-                var data = [];
-                var dataSeries = { type: "line", color: "red" };
-                var myDataPoints = [];
-                var myArray = sampleECG.split(" ").map(function (item) {
-                    return parseFloat(item);
-                });
+    /**
+    1. look in storageref for patientkey.txt file
+    2. download file
+    3. read file and convert into array of values
+    4. draw graph
+    */
+    var limit = 100000;    //increase number of dataPoints by increasing the limit
+    var y = 0;
+    var data = [];
+    var dataSeries = { type: "line", color: "black" };
+    var myDataPoints = [];
+    // Create a reference with an initial file path and name
+    var storageRef = storage.ref();
+    var pathReference = storageRef.child('ECGdata/'+patientKey+'.txt');
 
-                var time = 0;
-                var voltage = 0;
+    pathReference.getDownloadURL().then(function(url) {
+        // `url` is the download URL for 'images/stars.jpg'
 
-                for (var i = 0; i < myArray.length; i++) {
-                    if (i % 2 == 0) {
-                        time = myArray[i];
+        // This can be downloaded directly:
+        var xhr = new XMLHttpRequest();
+        /*
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+          var blob = xhr.response;
+          alert("sdsd");
+        };
+        */
+        xhr.open('GET', url);
+        xhr.responseType = 'text';
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {  // Makes sure the document is ready to parse.
+                if (xhr.status === 200) {  // Makes sure it's found the file.
+                    allText = xhr.responseText;
+                    lines = xhr.responseText.split("\n"); // Will separate each line into an array
+                    var time = 0;
+                    for (var i = 4; i < lines.length - 1; i++) {
+                          myDataPoints.push({
+                              x: time,
+                              y: parseFloat(lines[i])
+                          });
+                          time += 0.01;
+                          time = parseFloat(time.toFixed(3));
                     }
-                    else if (i % 2 == 1) {
-                        voltage = myArray[i];
-                        myDataPoints.push({
-                            x: time,
-                            y: voltage
-                        });
-                    }
+                    dataSeries.dataPoints = myDataPoints;
+                    data.push(dataSeries);
+                    console.log(data[0]); //object
+                    var chart = new CanvasJS.Chart("chartContainer", {
+                        zoomEnabled: true,
+                        animationEnabled: true,
+                        title: {
+                            text: "ECG"
+                        },
+                        axisX: {
+                            labelAngle: 30,
+                            title: "Time (seconds)",
+                            stripLines:xAxisStripLinesArray,
+                            gridThickness: 0,
+                            gridColor:"#FF0000",
+                            lineColor:"#FF0000",
+                            tickColor:"#FF0000",
+                            labelFontColor:"#FF0000",
+                        },
+                        axisY: {
+                            includeZero: false,
+                            labelAngle: 30,
+                            title: "Voltage (mV)",
+                            stripLines:yAxisStripLinesArray,
+                            gridThickness: 0,
+                            gridColor:"#FF0000",
+                            lineColor:"#FF0000",
+                            tickColor:"#FF0000",
+                            labelFontColor:"#FF0000",
+                        },
+                        data: data  // random data
+                    });
+                    addStripLines();
+                    chart.render();
+
+/*
+
+                    //Better to construct options first and then pass it as a parameter
+                    var options = {
+                        zoomEnabled: true,
+                        animationEnabled: true,
+                        title: {
+                            text: "ECG"
+                        },
+                        axisX: {
+                            labelAngle: 30,
+                            title: "Time (seconds)",
+                            stripLines:xAxisStripLinesArray,
+                            gridThickness: 0,
+                            gridColor:"#FF0000",
+                            lineColor:"#FF0000",
+                            tickColor:"#FF0000",
+                            labelFontColor:"#FF0000",
+                        },
+                        axisY: {
+                            includeZero: false,
+                            title: "Voltage (microVolts)",
+                            stripLines:yAxisStripLinesArray,
+                            gridThickness: 0,
+                            gridColor:"#FF0000",
+                            lineColor:"#FF0000",
+                            tickColor:"#FF0000",
+                            labelFontColor:"#FF0000",
+                            labelFontSize: 00
+                        },
+                        data: data  // random data
+                    };
+
+                    addStripLines();
+                    alert(data.length); //1
+                    console.log(data[0]);
+                    $("#chartContainer").CanvasJSChart(options).render();
+                    */
                 }
-
-                dataSeries.dataPoints = myDataPoints;
-                data.push(dataSeries);
-
-                //Better to construct options first and then pass it as a parameter
-                var options = {
-                    zoomEnabled: true,
-                    animationEnabled: true,
-                    title: {
-                        text: "ECG"
-                    },
-                    axisX: {
-                        labelAngle: 30,
-                        title: "Time (seconds)"
-                    },
-                    axisY: {
-                        includeZero: false,
-                        title: "Voltage (microVolts)"
-                    },
-                    data: data  // random data
-                };
-
-                $("#chartContainer").CanvasJSChart(options).render();
             }
-        });
+        };
+
+        xhr.send(null);
+
+    }).catch(function(error) {
+      // Handle any errors
     });
 
+}
+function addStripLines(){
+    for(var i=0; i<30; i=i+0.04){
+            xAxisStripLinesArray.push({value:i,thickness:0.5, color:"#FF0000"});
+    }
+    for(var i=0; i<30; i=i+0.2){
+            xAxisStripLinesArray.push({value:i,thickness:1.5, color:"#FF0000"});
+    }
+    for(var i=-5; i<5; i=i+0.1){
+            yAxisStripLinesArray.push({value:i,thickness:0.5, color:"#FF0000"});
+    }
+    for(var i= -5; i< 5; i=i+0.5){
+            yAxisStripLinesArray.push({value:i,thickness:1.5, color:"#FF0000"});
+    }
 }
 
 patientsRef.on("child_added", function(snapshot) {
